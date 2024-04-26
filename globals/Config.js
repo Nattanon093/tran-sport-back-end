8e82cb90a0697d2bf4c858e284e6d39eccca9a34
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2024-04-26 22:24:19 +0700

    add: Add Dockerfile

--- globals/Config.js
+++ globals/Config.js
@@ -2,8 +2,8 @@ const internal = {};
 
 export default internal.Global =  class {
     constructor() { 
-        this.SERVER_URL = 'http://localhost:5432' 
-        this.CLIENT_URL = 'http://localhost:5173'
+        this.SERVER_URL = 'https://api.bsdeliverymarket.com' 
+        this.CLIENT_URL = 'https://bsdeliverymarket.com'
     } 
  
      getServerUrl(){
